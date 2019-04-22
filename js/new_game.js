36400384bcb00df9e31b72405dc9f66e0ebeb09c
Riya Agrawal <43094144+ragrawal4545@users.noreply.github.com>
2019-04-22 19:14:11 -0400

    login complete

--- js/new_game.js
+++ js/new_game.js
@@ -1,8 +1,20 @@
 function createPlayer() {
 	var name = document.getElementById("playerName").value;
 	var diff = document.getElementById("difficultySelection").value;
+	var pilot = document.getElementById("pilotId").value;
+	var trader = document.getElementById("traderId").value;
+	var fighter = document.getElementById("fighterId").value;
+	var engineer = document.getElementById("engineerId").value;
+	var score = 1000;
+	var ship = "Gnat"
 	localStorage.setItem("name",name);
 	localStorage.setItem("diff",diff);
+	localStorage.setItem("pilot",pilot);
+	localStorage.setItem("trader",trader);
+	localStorage.setItem("fighter",fighter);
+	localStorage.setItem("engineer",engineer);
+	localStorage.setItem("score", score);
+	localStorage.setItem("ship", ship);
 	document.location.href = 'player_created.html';
     }
 
--- js/player_created.js
+++ js/player_created.js
@@ -1,4 +1,10 @@
 function playerCreated(){
 	document.getElementById("diff").innerHTML = localStorage.getItem("diff");
 	document.getElementById("name").innerHTML = localStorage.getItem("name");
+	document.getElementById("pilot").innerHTML = localStorage.getItem("pilot");
+	document.getElementById("fighter").innerHTML = localStorage.getItem("fighter");
+	document.getElementById("trader").innerHTML = localStorage.getItem("trader");
+	document.getElementById("engineer").innerHTML = localStorage.getItem("engineer");
+	document.getElementById("score").innerHTML = localStorage.getItem("score");
+	document.getElementById("ship").innerHTML = localStorage.getItem("ship");
 }
